07a0268e9b5d4279dc49dedf998868301e003b24
Francois Janson <jansonfrancois@gmail.com>
2021-09-30 17:29:56 +0200

    implementing POST routes

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const cors = require("cors")
 const app = express();
 const port = 8000;
+
 const debug = (req, res, next) => {
     console.log('Server responded to a request');
     next()
 }
+const transformName = (req, res, next) => {
+
+}
+
 const superHeroes = require("./superHeroes.json")
 
 app.use(cors());
@@ -19,20 +24,25 @@ app.get("/heroes", (req, res) => {
     res.json(superHeroes)
 })
 app.get("/heroes/:name", (req, res) => {
-    // console.log(`req.param`, req.params)
     const superHeroe = superHeroes.find(elem => elem.name.toLowerCase() === req.params.name.toLowerCase())
     // replace whitespace by `%20`
     res.json(superHeroe)
 })
 app.get("/heroes/:name/powers", (req, res) => {
-    // console.log(`req.param`, req.params)
-    const superHeroe = superHeroes.find(elem => elem.name.toLowerCase() === req.params.name.toLowerCase())
     // replace whitespace by `%20`
+    const superHeroe = superHeroes.find(elem => elem.name.toLowerCase() === req.params.name.toLowerCase())
     res.json(superHeroe.power)
 })
 app.post("/heroes", (req, res) => {
-
+    superHeroes.push(req.body)
     res.send("Ok, héro ajouté !")
+    console.log(`superHeroes`, superHeroes)
+})
+app.post("/heroes/:name/powers", (req, res) => {
+    const superHeroe = superHeroes.find(elem => elem.name.toLowerCase() === req.params.name.toLowerCase())
+    superHeroe.power.push(req.body.power)
+    res.send("Ok, pouvoir ajouté !")
+    console.log(`superHeroes`, superHeroes)
 })
 
 app.listen(port, () => {
